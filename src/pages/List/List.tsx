04851b9ca5ede2a09ff66f61c6c8493e1157853f
Matan Shaviro <matan3sh@gmail.com>
2022-12-19 17:20:58 +0200

    Implement DataTable component

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,7 +1,18 @@
+import DataTable from "../../components/DataTable/DataTable";
+import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import "./List.scss";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <DataTable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
